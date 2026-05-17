e846ffe0862fec33979a9269421630804c2bbc4b
Bakhtovarkhuja <shodmonovbakha4@gmail.com>
2026-05-17 10:39:29 +0500

    create files

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Container from '@/app/components/container/container'
+import Header from '@/app/widgets/header/header'
+import Footer from '@/app/widgets/footer/footer'
+
+export default function Home(){
+  return(
+    <Container>
+      <Header></Header>
+      <Footer></Footer>
+    </Container>
+  )
+}
