70cd59c2ce044e404eec5d7e1ae305d299490d29
shodmonovolimkhuja1-maker <shodmonovolimkhuja1@gmail.com>
2026-05-19 13:38:47 +0500

    create page about

--- src/app/components/buttons/button1.jsx
+++ src/app/components/buttons/button1.jsx
@@ -10,7 +10,7 @@ export default function Button({ children }) {
 				animate-[gradientMove_5s_linear_infinite]
 			"
 		>
-			<button className="px-4 py-1 rounded-[7px] bg-[rgb(24,4,34)] text-white">
+			<button className="px-4 py-2 rounded-[7px] bg-[rgb(24,4,34)] text-white">
 				{children}
 			</button>
 		</div>
